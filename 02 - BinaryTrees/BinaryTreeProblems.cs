91e03e8c08c5f3c72adee3b1b205b8e0fdd1cdb5
Alexander Liedtke <e.alexander.liedtke@gmail.com>
2020-12-02 13:48:29 -0500

    Added problems 1 and 2 for BinaryTreeProblems

---
+++ 02 - BinaryTrees/BinaryTreeProblems.cs
@@ -0,0 +1,100 @@
+using System;
+
+//The Node class, used to set up the binary trees
+public class Node
+{
+    public Node LeftNode { get; set; }
+    public Node RightNode { get; set; }
+    public int Data { get; set; }
+}
+
+public class BinaryTreeProblems {
+
+    public static void Main() {
+        //Setup:  Make the following Binary Tree
+        //                0
+        //            /       \
+        //          1           2
+        //        /   \       /   \
+        //      3       4   5       6
+        //    /   \
+        //  7       8		
+		Node[] arr = new Node[9];
+        for (int i = 0; i <= 8; i++) 
+		{
+			Node temp = new Node();
+			temp.Data = i;
+			arr[i] = temp;
+		}
+		
+		arr[0].LeftNode = arr[1];
+        arr[0].RightNode = arr[2];
+        arr[1].LeftNode = arr[3];
+        arr[1].RightNode = arr[4];
+        arr[2].LeftNode = arr[5];
+        arr[2].RightNode = arr[6];
+        arr[3].LeftNode = arr[7];
+        arr[3].RightNode = arr[8];
+        
+        
+        //PROBLEM #1:
+        //  Write a function where you pass it a node and it returns the sum of the depths of the nodes beneath it.
+		//  In the setup tree this would mean nodes 5 and 6 each have a depth of 1 relative to node 2, so DepthSum(Node 2) = 2 which is 1 + 1.
+		//  A more complicated example would be DepthSum(Node 1) = 6, since nodes 3 and 4 are depth 1 each, and nodes 7 and 8 are depth 2 each.
+		
+		Console.WriteLine(DepthSum(arr[0]));  //Should be 16
+		Console.WriteLine(DepthSum(arr[3]));  //Should be 2
+		Console.WriteLine(DepthSum(arr[1]));  //Should be 6
+		Console.WriteLine(DepthSum(arr[5]));  //Should be 0
+		
+		//Problem #2:
+		//  Building on Problem #1, write a function that returns the sum of the DepthSums of all nodes in the tree.
+		//  In the setup tree this would mean AllDepthSums(Node 1) = 8, since DepthSum(Node 3) = 2, DepthSum(1) = 6, and all other DepthSums are 0.
+		Console.WriteLine(AllDepthSums(arr[0]));  //Should be 26
+		Console.WriteLine(AllDepthSums(arr[1]));  //Should be 8
+		
+		//Problem #3:
+		//  Continuing with the theme, write a function where you pass it a node and it returns the sum of the distances from all other nodes in the tree.
+		//  In the setup tree this would mean DistanceSum(Node 0) = 16 still, but if you called DistanceSum(Node 1) the answer would be 15, since Node 0 is
+		//  a distance of 1 from Node 1, Node 2 is distance 2, and Nodes 5 and 6 are distance 3
+		Console.WriteLine(DistanceSum(arr[0]));  //Should be 16
+		Console.WriteLine(DistanceSum(arr[1]));  //Should be 15
+		Console.WriteLine(DistanceSum(arr[4]));  //Should be 22
+		Console.WriteLine(DistanceSum(arr[7]));  //Should be 25
+    }
+	
+	//ANSWER #1:
+	public static int DepthSum(Node head) {
+		return DepthSum(head, 0);
+	}
+	
+	public static int DepthSum(Node currentNode, int currentDepth) {
+		int tempVal = currentDepth;
+		if (currentNode.LeftNode != null) { 
+			tempVal += DepthSum(currentNode.LeftNode, currentDepth + 1);
+		}
+		if (currentNode.RightNode != null) { 
+			tempVal += DepthSum(currentNode.RightNode, currentDepth + 1);
+		}
+		return tempVal;
+	}
+	
+	//ANSWER #2:
+	public static int AllDepthSums (Node currentNode) {
+		int tempVal = DepthSum(currentNode);
+		if (currentNode.LeftNode != null) { 
+			tempVal += AllDepthSums(currentNode.LeftNode);
+		}
+		if (currentNode.RightNode != null) { 
+			tempVal += AllDepthSums(currentNode.RightNode);
+		}
+		return tempVal;
+	}
+	
+	//ANSWER #3:
+	public static int DistanceSum(Node currentNode){
+		//TODO: Write function
+		return 0;
+	}
+	
+}
